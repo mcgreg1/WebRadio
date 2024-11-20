1fb31e6cd91e63fcc2d999bd2dc864ccfa740040
mcgreg1 <44223921+mcgreg1@users.noreply.github.com>
2024-11-20 08:53:44 +0100

    Cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -2,30 +2,18 @@
 #include <Preferences.h> //write to flash
 #include "WiFi.h"
 #include "Audio.h"
-#include <SimpleRotary.h>
+#include <SimpleRotary.h> 
 //#include <WebServer.h>
-#include "BluetoothA2DPSink.h"
-#include "Ticker.h"
-
-#define SHUTDOWN_TIME 10 //deepsleep after 10 min. 
-
-BluetoothA2DPSink a2dp_sink;
-//WebServer server(80); // Server der App
-Preferences prefs; 
-//store in flash: 
-//conn_mode, volume, sender, active
-ushort conn_mode=0; // 0 = Webradio, 1 = Bluetooth-Audio
-ushort audioVolume=10;
-ushort currentStation=0;
-
-
+#include "BluetoothA2DPSink.h" //for Bluetooth mode
+#include "Ticker.h" //for minute periodic timer
 
 //OLED
 #include <Wire.h>
 #include <Adafruit_SSD1306.h>
 
-Adafruit_SSD1306 display(128, 32, &Wire, -1);
-
+#define SHUTDOWN_TIME 10 //deepsleep after 10 min. 
+#define WIFI_TIMEOUT_MS 20000 // So lange wird versucht, neu mit Wifi zu verbinden, wenn die Verbindung mal verloren geht (in keepWiFiAlive())
+#define MAX_AUDIO_VOLUME 40 //pay attention to the maxvolumesteps and Bluetooth scaling factor
 // Digital I/O used
 
 #define I2S_DOUT      14
@@ -33,16 +21,17 @@ Adafruit_SSD1306 display(128, 32, &Wire, -1);
 #define I2S_LRC       26
 
 
+BluetoothA2DPSink a2dp_sink;
+//WebServer server(80); // TODO: Webserver
+Preferences prefs; 
+Adafruit_SSD1306 display(128, 32, &Wire, -1);
 Audio audio;
 
+//TODO: credentials via Webserver
 String ssid =     "Pandora2";
 String password = "LetMeIn#123";
-#define WIFI_TIMEOUT_MS 20000 // So lange wird versucht, neu mit Wifi zu verbinden, wenn die Verbindung mal verloren geht (in keepWiFiAlive())
-
-
 
 SimpleRotary rotary(18,19,15);
-
 
 //structure for station list
 typedef struct {
@@ -57,6 +46,13 @@ Station stationlist[STATIONS] PROGMEM = {
 {"http://stream.antenne.de/antenne/stream/mp3","ANTENNE"},
 {"http://streams.br.de/br24_2.m3u","BR24"}};
 
+
+//Values stored in flash
+ushort conn_mode=0; // 0 = Webradio, 1 = Bluetooth-Audio
+ushort audioVolume=10;
+ushort currentStation=0;
+
+
 String currentSong;
 bool wifi_conn_success=false;
 unsigned long changeTime; 
@@ -65,8 +61,6 @@ bool audioChanged;
 bool isPlaying; //for Bluetooth only
 
 Ticker minuteTicker;
-
-
 
 void keepWiFiAlive(void * parameters) {
   for (;;) {
@@ -100,7 +94,6 @@ void keepWiFiAlive(void * parameters) {
 
 void drawMainMenu()
 {
-  //if (audioVolume)
   {
     display.clearDisplay();
     display.setCursor(0,0);             // Start at top-left corner
@@ -108,9 +101,6 @@ void drawMainMenu()
     if (conn_mode==0)
     {
       display.println(stationlist[currentStation].name);
-      //display.setCursor(0,80);
-      //display.println("22:55");
-      //display.setCursor(16,0);
       display.setTextSize(1);
       //split the interpret and songname
       byte delim = currentSong.indexOf('-');
@@ -125,8 +115,6 @@ void drawMainMenu()
       display.println("Bluetooth");
       display.println("  mode");
     }
-
-
     display.display();
   }
 }
@@ -139,8 +127,6 @@ void drawProgressbar(int x,int y, int width,int height, int progress)
    float bar = ((float)(width-1) / 100) * progress;
    display.drawRect(x, y, width, height, SSD1306_WHITE);
    display.fillRect(x+2, y+2, bar , height-4, SSD1306_WHITE); // initailize the graphics fillRect(int x, int y, int width, int height)
-   
-
 }
 
 void readFromFlash()
@@ -148,7 +134,9 @@ void readFromFlash()
     conn_mode= prefs.getUShort("ConnMode", 0);
     audioVolume= prefs.getUShort("volume", audioVolume);
     currentStation= prefs.getUShort("station", 0);
-    Serial.printf("--------------------------------------------------\nFrom flash: mode: %d, volume: %d, station: %d\n", conn_mode, audioVolume, currentStation);
+    Serial.printf("-----------------------------------------------------\n");
+    Serial.printf("From flash: mode: %d, volume: %d, station: %d\n", conn_mode, audioVolume, currentStation);
+    Serial.printf("-----------------------------------------------------\n");
 }
 
 void writeToFlash()
@@ -156,20 +144,20 @@ void writeToFlash()
     prefs.putUShort("ConnMode", conn_mode);
     prefs.putUShort("volume", audioVolume);
     prefs.putUShort("station", currentStation);
-    Serial.printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\nTo flash: mode: %d, volume: %d, station: %d\n", conn_mode, audioVolume, currentStation);
+    Serial.printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n);
+    Serial.printf("To flash: mode: %d, volume: %d, station: %d\n", conn_mode, audioVolume, currentStation);
+    Serial.printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n);
 }
 
+//Callback for the Bluetooth mode, when data is received, Workaround to see if music is streaming or idle
 void data_received_callback() {
   //Serial.println("Data packet received");
-
   if (audioVolume>0)
       dataPacketReceivedTime=millis();
-  
 }
 
 void minuteCheck()
 {
-
    Serial.printf("Minute check, uptime %d minutes\n", (millis()/60000));
 
   int inactiveMinutes = (millis()-dataPacketReceivedTime)/60000;
@@ -183,7 +171,7 @@ void minuteCheck()
     display.printf("%d minutes", SHUTDOWN_TIME-inactiveMinutes);
     display.display();
   }
-  if (inactiveMinutes>SHUTDOWN_TIME)
+  if (inactiveMinutes>=SHUTDOWN_TIME)
   {
     Serial.println("Going to deepsleep");
     conn_mode=0; //start always in webradio 
@@ -198,20 +186,14 @@ void minuteCheck()
 
 }
 
-
-
-void setup() {
-
-  pinMode(12, OUTPUT);
-  digitalWrite(12, LOW);
+void setup() 
+{
   esp_sleep_enable_ext0_wakeup(GPIO_NUM_15, 0); //Rotary Button as wakeup 
-
-
   Serial.begin(9600);
+
   //load preferences
   prefs.begin("webradio");
   readFromFlash();
-  //conn_mode=0; //start always in web radio mode
 
   if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3c)) {
     Serial.println(F("SSD1306 allocation failed"));
@@ -223,10 +205,6 @@ void setup() {
   display.setCursor(0,0);             // Start at top-left corner
   display.println("ESP Startup");
   display.display();
-  //display.drawString(0, 16, "Hello world");
-
-  display.display();
-
 
   if (conn_mode==0)
   {
@@ -241,9 +219,9 @@ void setup() {
     Serial.println("connected!");
     display.println("connected!");
 
-
       // WiFi-Verbindung aufrechterhalten
-    xTaskCreatePinnedToCore(
+    xTaskCreatePinnedToCore
+    (
       keepWiFiAlive,
       "Keep Wifi Alive",
       5000,
@@ -255,14 +233,9 @@ void setup() {
   }
   if (conn_mode==1)
   {
-    /*
-#define I2S_DOUT      14 //25??
-#define I2S_BCLK      27
-#define I2S_LRC       26
-*/
-
     Serial.println("Bluetooth Mode");
-    i2s_pin_config_t my_pin_config = {
+    i2s_pin_config_t my_pin_config = 
+    {
     .bck_io_num = I2S_BCLK,
     .ws_io_num = I2S_LRC,
     .data_out_num = I2S_DOUT,
@@ -283,8 +256,6 @@ void setup() {
       display.println("Mode");
     }
     display.display();
-
-
   }
   audio.setPinout(I2S_BCLK, I2S_LRC, I2S_DOUT);
   audio.setVolumeSteps(64); // max 255
@@ -295,22 +266,14 @@ void setup() {
       audio.connecttohost(stationlist[currentStation].url.c_str());
       Serial.printf(" Current station: %s\n, index=%d\n", stationlist[currentStation].name, currentStation);
   }
-
-
-
-  //delay(1000);
-
   minuteTicker.attach_ms(60000, minuteCheck);
-
 }
 
-void loop(){
-
+void loop()
+{
   //Rotary
   byte btn;
   // 0 = not pushed, 1 = pushed, 2 = long pushed
-
-  //button only in wifi mode fdse
   btn = rotary.pushType(1000); // number of milliseconds button has to be pushed for it to be considered a long push.
   if ( btn == 1) 
   {
@@ -335,7 +298,6 @@ void loop(){
       isPlaying= !isPlaying;
       Serial.printf("Setting is playing to %d\n", isPlaying);
     }
-
   }
 
   if ( btn == 2 ) 
@@ -346,74 +308,51 @@ void loop(){
     else 
       conn_mode=0;
     writeToFlash();
-    //delay(100);
+    prefs.end();
+    delay(100);
     ESP.restart();
-    /*
-    if (isPlaying)
-    {
-      Serial.println("Is Playing, shutdown");
-      audio.connecttohost(NULL);
-    }
-    else
-    {
-      Serial.println("Wakeup");
-    }
-    */
   }
   audio.loop();
   
-
   byte rot;
   rot = rotary.rotate();
   
   // CW
   if ( rot == 1 )
   {
-    if (audioVolume<40)
-    {
+    if (audioVolume<MAX_AUDIO_VOLUME)
       audio.setVolume(++audioVolume);
-      
-    }
-      
-    Serial.printf("Audio: %d\n", audioVolume);
   }
   // CCW
   if ( rot == 2 ) 
   {
     if (audioVolume>0)
-    {
       audio.setVolume(--audioVolume);
-    }
-      
-    Serial.printf("Audio: %d\n", audioVolume);
-
   }
-
+  //Display status on any rotation
   if (rot>0)
   {
-      display.display();
+      Serial.printf("Audio: %d\n", audioVolume);
       changeTime = millis();
       audioChanged=true;
       a2dp_sink.set_volume(audioVolume*2.5); //bluetooth scaling factor
       //void drawProgressbar(int x,int y, int width,int height, int progress)
+      // (colour == Red)? return red : return !red;
       int progress = audioVolume*2.5;
-      if (conn_mode==1)
-        progress = progress*2.5;
+
       display.clearDisplay();
       drawProgressbar(0, 0, 120, 15 ,progress);
       display.setCursor(5, 21);
       display.setTextSize(1);
       display.printf("   Volume: %d %%\n", progress);
       display.display();
-
   }
-
+  //Keep the audio volume on display for 5 seconds
   if (millis()-changeTime>5*1000)
   {
     drawMainMenu();
-    
   }
-  //save audio after 10 seconds
+  //save audio after 10 seconds only, to avoid too many flash writes
   if ((millis()-changeTime)>10*1000 && audioChanged)
   {
     Serial.printf("Write to flash because of audio change to %d\n", audioVolume);
@@ -423,55 +362,28 @@ void loop(){
       
     }
     audioChanged=false;
-
   }
-
-
-   
     //vTaskDelay(1);
 }
 
-// optional
-void audio_info(const char *info){
+// Web radio events
+void audio_info(const char *info)
+{
     Serial.print("info        "); Serial.println(info);
     if (audioVolume>0)
       dataPacketReceivedTime=millis();
 }
-void audio_showstreamtitle(const char *info){
+void audio_showstreamtitle(const char *info)
+{
   currentSong = String(info);
   drawMainMenu();
   Serial.print("streamtitle ");Serial.println(info);
   if (audioVolume>0)
     dataPacketReceivedTime=millis();
 }
-void audio_id3data(const char *info){  //id3 metadata
-    Serial.print("id3data     ");Serial.println(info);
-}
-void audio_eof_mp3(const char *info){  //end of file
-    Serial.print("eof_mp3     ");Serial.println(info);
-}
-void audio_showstation(const char *info){
+
+void audio_showstation(const char *info)
+{
   drawMainMenu();
     Serial.print("station     ");Serial.println(info);
-
-
 }
-
-void audio_bitrate(const char *info){
-    Serial.print("bitrate     ");Serial.println(info);
-}
-void audio_commercial(const char *info){  //duration in sec
-    Serial.print("commercial  ");Serial.println(info);
-}
-void audio_icyurl(const char *info){  //homepage
-    Serial.print("icyurl      ");Serial.println(info);
-}
-void audio_lasthost(const char *info){  //stream URL played
-    Serial.print("lasthost    ");Serial.println(info);
-}
-void audio_eof_speech(const char *info){
-    Serial.print("eof_speech  ");Serial.println(info);
-}
-
-
-
